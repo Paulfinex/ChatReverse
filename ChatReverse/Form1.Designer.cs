4e62b626f06e7b81393448fe88e71f5b7702dce2
Giovanni <Giovanni@LAPTOP-J9SK0Q1U>
2021-07-26 15:56:04 +0200

    Add project files.

---
+++ ChatReverse/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace ChatReverse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_select_input_path = new System.Windows.Forms.Button();
+            this.dialog_input_fldr = new System.Windows.Forms.FolderBrowserDialog();
+            this.btn_revert = new System.Windows.Forms.Button();
+            this.btn_select_output_path = new System.Windows.Forms.Button();
+            this.dialog_output_fldr = new System.Windows.Forms.FolderBrowserDialog();
+            this.worker_reverse = new System.ComponentModel.BackgroundWorker();
+            this.prog_bar_reverse = new System.Windows.Forms.ProgressBar();
+            this.lbl_perc = new System.Windows.Forms.Label();
+            this.txt_input_path = new System.Windows.Forms.TextBox();
+            this.txt_output_path = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_select_input_path
+            // 
+            this.btn_select_input_path.Location = new System.Drawing.Point(10, 25);
+            this.btn_select_input_path.Name = "btn_select_input_path";
+            this.btn_select_input_path.Size = new System.Drawing.Size(117, 24);
+            this.btn_select_input_path.TabIndex = 2;
+            this.btn_select_input_path.Text = "Cartella Origine";
+            this.btn_select_input_path.UseVisualStyleBackColor = true;
+            this.btn_select_input_path.Click += new System.EventHandler(this.btn_select_input_path_Click);
+            // 
+            // btn_revert
+            // 
+            this.btn_revert.Location = new System.Drawing.Point(10, 83);
+            this.btn_revert.Name = "btn_revert";
+            this.btn_revert.Size = new System.Drawing.Size(117, 23);
+            this.btn_revert.TabIndex = 5;
+            this.btn_revert.Text = "Inverti Chat";
+            this.btn_revert.UseVisualStyleBackColor = true;
+            this.btn_revert.Click += new System.EventHandler(this.btn_revert_Click);
+            // 
+            // btn_select_output_path
+            // 
+            this.btn_select_output_path.Location = new System.Drawing.Point(10, 55);
+            this.btn_select_output_path.Name = "btn_select_output_path";
+            this.btn_select_output_path.Size = new System.Drawing.Size(117, 24);
+            this.btn_select_output_path.TabIndex = 6;
+            this.btn_select_output_path.Text = "Cartella Destinazione";
+            this.btn_select_output_path.UseVisualStyleBackColor = true;
+            this.btn_select_output_path.Click += new System.EventHandler(this.btn_select_output_path_Click);
+            // 
+            // worker_reverse
+            // 
+            this.worker_reverse.WorkerReportsProgress = true;
+            this.worker_reverse.DoWork += new System.ComponentModel.DoWorkEventHandler(this.worker_reverse_DoWork);
+            this.worker_reverse.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.worker_reverse_ProgressChanged);
+            this.worker_reverse.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.worker_reverse_RunWorkerCompleted);
+            // 
+            // prog_bar_reverse
+            // 
+            this.prog_bar_reverse.Location = new System.Drawing.Point(133, 83);
+            this.prog_bar_reverse.Name = "prog_bar_reverse";
+            this.prog_bar_reverse.Size = new System.Drawing.Size(186, 23);
+            this.prog_bar_reverse.TabIndex = 8;
+            // 
+            // lbl_perc
+            // 
+            this.lbl_perc.AutoSize = true;
+            this.lbl_perc.Location = new System.Drawing.Point(142, 110);
+            this.lbl_perc.Name = "lbl_perc";
+            this.lbl_perc.Size = new System.Drawing.Size(0, 13);
+            this.lbl_perc.TabIndex = 9;
+            // 
+            // txt_input_path
+            // 
+            this.txt_input_path.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_input_path.Location = new System.Drawing.Point(133, 28);
+            this.txt_input_path.Name = "txt_input_path";
+            this.txt_input_path.ReadOnly = true;
+            this.txt_input_path.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txt_input_path.Size = new System.Drawing.Size(272, 19);
+            this.txt_input_path.TabIndex = 10;
+            // 
+            // txt_output_path
+            // 
+            this.txt_output_path.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_output_path.Location = new System.Drawing.Point(133, 58);
+            this.txt_output_path.Name = "txt_output_path";
+            this.txt_output_path.ReadOnly = true;
+            this.txt_output_path.Size = new System.Drawing.Size(272, 19);
+            this.txt_output_path.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 150);
+            this.Controls.Add(this.txt_output_path);
+            this.Controls.Add(this.txt_input_path);
+            this.Controls.Add(this.lbl_perc);
+            this.Controls.Add(this.prog_bar_reverse);
+            this.Controls.Add(this.btn_select_output_path);
+            this.Controls.Add(this.btn_revert);
+            this.Controls.Add(this.btn_select_input_path);
+            this.Name = "Form1";
+            this.Text = "Inverti Chat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_select_input_path;
+        private System.Windows.Forms.FolderBrowserDialog dialog_input_fldr;
+        private System.Windows.Forms.Button btn_revert;
+        private System.Windows.Forms.Button btn_select_output_path;
+        private System.Windows.Forms.FolderBrowserDialog dialog_output_fldr;
+        private System.ComponentModel.BackgroundWorker worker_reverse;
+        private System.Windows.Forms.ProgressBar prog_bar_reverse;
+        private System.Windows.Forms.Label lbl_perc;
+        private System.Windows.Forms.TextBox txt_input_path;
+        private System.Windows.Forms.TextBox txt_output_path;
+    }
+}
+
---
+++ ChatReverse/Form1.cs
@@ -0,0 +1,169 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatReverse
+{
+    public partial class Form1 : Form
+    {
+
+        string input_path = "";
+        string output_path = "";
+        List<string> lista_piattaforme = new List<string>() {"","Instagram" };
+        public Form1()
+        {
+            InitializeComponent();
+            prog_bar_reverse.Visible = false;
+            lbl_perc.Text = "";
+            //combo_piattaforma.DataSource = lista_piattaforme;
+        }
+
+        private void btn_select_input_path_Click(object sender, EventArgs e)
+        {
+            dialog_input_fldr.ShowDialog();
+            input_path = dialog_input_fldr.SelectedPath;
+            txt_input_path.AppendText(dialog_input_fldr.SelectedPath);
+
+
+        }
+
+        private void btn_select_output_path_Click(object sender, EventArgs e)
+        {
+             
+                dialog_output_fldr.ShowDialog();
+                string[] files = Directory.GetFiles(dialog_output_fldr.SelectedPath, "*.*", SearchOption.AllDirectories);
+            if (files.Length > 0)
+            {
+                DialogResult dialogResult = MessageBox.Show("La cartella di destinazione selezionata contiene già dei file, continuare comunque?", "Struttura cartella già esistente", MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+                if (dialogResult == DialogResult.Yes)
+                {
+
+                    output_path = dialog_output_fldr.SelectedPath;
+                    txt_output_path.AppendText(dialog_output_fldr.SelectedPath);
+                }
+
+                if (dialogResult == DialogResult.No)
+                {
+                    return;
+                }
+            }
+            else
+            {
+                output_path = dialog_output_fldr.SelectedPath;
+                txt_output_path.AppendText(dialog_output_fldr.SelectedPath);
+            }
+            
+        }
+
+        private void btn_revert_Click(object sender, EventArgs e)
+        {
+            if (txt_input_path.Text == "" || txt_output_path.Text == "")
+            {
+                MessageBox.Show("Selezionare entrambi i percorsi prima di procedere.");
+                return;
+            }
+            if(!worker_reverse.IsBusy)
+            {
+                prog_bar_reverse.Visible = true;
+                worker_reverse.RunWorkerAsync();
+
+            }
+        }
+
+        private void worker_reverse_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int currLine = 1;
+            int totLines = 1;
+            foreach (string s in Directory.GetDirectories(input_path, "*", SearchOption.AllDirectories))
+            {
+                totLines += 1;
+            }
+            foreach (string s in Directory.GetFiles(input_path, "*.*", SearchOption.AllDirectories))
+            {
+                totLines += 1;
+            }
+            foreach (string s in Directory.GetFiles(input_path, "*.html", SearchOption.AllDirectories))
+            {
+                if (s.Contains("message_"))
+                {
+                    totLines += 1;
+                }
+            }
+
+            //Now Create all of the directories
+            foreach (string dirPath in Directory.GetDirectories(input_path, "*", SearchOption.AllDirectories))
+            {
+                
+                Directory.CreateDirectory(dirPath.Replace(input_path, output_path));
+
+                worker_reverse.ReportProgress(currLine++ * 100 / totLines);
+            }
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles(input_path, "*.*", SearchOption.AllDirectories))
+            {
+                File.Copy(newPath, newPath.Replace(input_path, output_path), true);
+
+                worker_reverse.ReportProgress(currLine++ * 100 / totLines);
+            }
+
+
+            string[] htmlFiles = Directory.GetFiles(output_path, "*.html", SearchOption.AllDirectories);
+            foreach (string s in htmlFiles)
+            {
+                if (s.Contains("message_"))
+                {
+                    try
+                    {
+                        string readText = File.ReadAllText(s);
+                        readText = readText.Replace("._4t5n{", "._4t5n{display:flex;flex-direction:column-reverse;");
+                        File.WriteAllText(s, string.Empty);
+                        File.WriteAllText(s, readText);
+                    }
+                    catch(Exception Read_Write_exception)
+                    {
+                        MessageBox.Show("Errore nella modifica dei file HTML: " + Environment.NewLine + Read_Write_exception);
+
+                    }
+
+                    worker_reverse.ReportProgress(currLine++ * 100 / totLines);
+                }
+            }
+            worker_reverse.ReportProgress(currLine++ * 100 / totLines);
+        }
+
+        private void worker_reverse_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            prog_bar_reverse.Value = e.ProgressPercentage;
+            lbl_perc.Text = string.Format("{0}%", e.ProgressPercentage);
+            prog_bar_reverse.Update();
+        }
+
+        private void worker_reverse_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Inversione chat completata, output disponibile in:" + Environment.NewLine + output_path);
+            lbl_perc.Invoke((MethodInvoker)delegate { lbl_perc.Text = ""; });
+            lbl_perc.Invoke((MethodInvoker)delegate { prog_bar_reverse.Visible = false; });
+            if (Directory.Exists(output_path))
+            {
+                Process.Start(output_path);
+            }
+        }
+
+        private static void CopyFilesRecursively(string sourcePath, string targetPath)
+        {
+            
+        }
+
+    
+    }
+}
